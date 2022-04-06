238e9da0b8c381a62fa1239bdf40cc84c091be6e
Alexis Ocampo <al3xisoc4mpo@gmail.com>
2022-04-06 11:48:13 -0500

    classes.ts completed

---
+++ classes.ts
@@ -0,0 +1,34 @@
+class Vehicle {
+  //   public drive(): void {
+  //     console.log("chugga chugga");
+  //   }
+  //   color: string = "red";
+  constructor(public color: string) {
+    this.color = color;
+  }
+  protected honk(): void {
+    console.log("beep");
+  }
+}
+
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+// vehicle.drive();
+// vehicle.honk();
+
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  private drive(): void {
+    console.log("vroom");
+  }
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(4, "red");
+car.startDrivingProcess();
+// car.honk();
